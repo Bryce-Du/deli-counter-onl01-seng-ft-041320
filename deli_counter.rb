4ebc9b82a696b01e21a3538413d1fde4959a790a
Bryce Dumas <bryce.dumas@gmail.com>
2020-04-15 11:07:56 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,16 @@
 # Write your code here.
+def line(deli_line)
+  message = "The line is currently"
+  if deli_line.length == 0
+    message << " empty."
+  else
+    message << ": "
+    deli_line.each_with_index do |customer, index|
+      message << "#{index + 1}. #{customer}"
+      if index < deli_line.length
+        message << " "
+      end
+    end
+  end
+  puts message
+end
